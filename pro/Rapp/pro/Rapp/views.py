590e7546f79356208b46db49d716a2827112b66e
Rajrami1 <139840431+Rajrami1@users.noreply.github.com>
2023-07-25 11:12:41 +0530

    Add files via upload

---
+++ pro/Rapp/pro/Rapp/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request,'home.html')
+
+def india(request):
+    return render(request,'india.html')
+
+def australia(request):
+    return render(request,'australia.html')
+
+def america(request):
+    return render(request,'america.html')
+
+def gujarat(request):
+    return render(request,'gujarat.html')
+
+def maharashtra(request):
+    return render(request,'maharashtra.html')
+
+def karnataka(request):
+    return render(request,'karnataka.html')
+
+def gandhinagar(request):
+    return render(request,'gandhinagar.html')
+
+def mumbai(request):
+    return render(request,'mumbai.html')
+
+def bengaluru(request):
+    return render(request,'bengaluru.html')
+
+def california(request):
+    return render(request,'california.html')
+
+def florida(request):
+    return render(request,'florida.html')
+
+def georgia(request):
+    return render(request,'georgia.html')
+#
+# def gandhinagar(request):
+#     return render(request,'gandhinagar.html')
+#
+# def gandhinagar(request):
+#     return render(request,'gandhinagar.html')
